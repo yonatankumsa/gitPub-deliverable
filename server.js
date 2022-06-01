d295b24808be27dc83a0d718f8c5d5c4960c5ecc
yonatankumsa <yonatankumsa99@gmail.com>
2022-06-01 16:37:04 -0400

    first commit

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const app = require("liquid-express-views")(express())
+// const app = express()
 
 app.listen(3000, ()=>{
     console.log("listnening on port 3k")
 })
 
 
+app.get("/", (req,res)=>{
+    res.send("Welcome to the Gitpub App!")
+})
